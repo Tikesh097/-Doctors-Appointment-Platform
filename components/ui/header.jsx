dde0515ab8e382565c3a4dc74e886ec98efe1d86
Tikesh <aswaletinku@gmail.com>
2026-03-26 03:08:38 +0530

    Backend Auth work-ongoing

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -2,9 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
+import { checkUser } from '@/lib/checkUser';
 
 
-const Header = () => {
+const Header = async () => {
+    await checkUser()
     return (
         <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-backdrop-filter:bg-background/60'>
             <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
